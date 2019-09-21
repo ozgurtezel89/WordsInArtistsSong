c46a0c30641912cb7f50017f7165e0ddc4eaf241
Ozgur Tezel <ozgurtezel89@gmail.com>
2019-09-21 21:29:51 +0100

    search form to get artist name from user created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechTest.Models;
 
 namespace TechTest.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(new AverageWordsInSongSearchViewModel());
         }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(AverageWordsInSongSearchViewModel viewModel)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
---
+++ Models/AverageWordsInSongSearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechTest.Models
+{
+    public class AverageWordsInSongSearchViewModel
+    {
+        [Display(Name = "Artist Name:")]
+        [Required]
+        [StringLength(100)]
+        public string ArtistNameToSearch { get; set; }
+    }
+}
